7f0353927f1d30ab85da85bedd68a496f2f448c3
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2023-04-21 02:56:58 -0500

    created: files to be edited
    .h and .cpp files:
    Character, Field, Item, FireArm
    .cpp files:
    Driver

---
+++ src/Driver.cpp
@@ -0,0 +1,22 @@
+/*
+ * Driver.cpp
+ *
+ * Xcom-Mimic
+ * 
+ * @author      Hugo Ng
+ * @version     21-April-2023
+ *
+ * PURPOSE:     The driver of Xcom-Mimic
+ */
+
+#include "Driver.h"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+	//main method to run the driver
+	std::cout << "Xcom-Mimic begins..." << std::endl;
+
+
+
+	std::cout << "Xcom-Mimic terminates normally." << std::endl;
+}
